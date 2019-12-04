5a22289ac399332897c4ba3f7d93a46281ed1b64
justinm1295 <myersonj@gmail.com>
2019-12-03 22:21:15 -0500

    Updated channel mention.

--- src/main/java/Commands/CommandList.java
+++ src/main/java/Commands/CommandList.java
@@ -1,10 +1,13 @@
 package Commands;
 
+import Bot.FireBot;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import java.util.Objects;
 
 public class CommandList {
 
     public static void sendCommandList(MessageReceivedEvent event) {
-        event.getChannel().sendMessage("Please see #bot-commands for the full list of commands for this server.").queue();
+        event.getChannel().sendMessage(String.format("Please see %s for the full list of commands for this server.", Objects.requireNonNull(FireBot.jda.getTextChannelById(651617393323409419L)).getAsMention())).queue();
     }
 }
